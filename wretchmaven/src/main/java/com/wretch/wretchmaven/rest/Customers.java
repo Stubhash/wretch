f058b972496a95406fa25f494cfcaa2a1582facf
kim.stuber <kim.stuber@web.de>
2019-12-04 21:22:15 +0100

    database

---
+++ wretchmaven/src/main/java/com/wretch/wretchmaven/rest/Customers.java
@@ -0,0 +1,10 @@
+package com.wretch.wretchmaven.rest;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="Customers")
+public class Customers {
+
+}
--- wretchmaven/src/main/java/com/wretch/wretchmaven/rest/LoginRestController.java
+++ wretchmaven/src/main/java/com/wretch/wretchmaven/rest/LoginRestController.java
@@ -1,7 +1,8 @@
 package com.wretch.wretchmaven.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping("/")
 @RestController
 public class LoginRestController {
 	
