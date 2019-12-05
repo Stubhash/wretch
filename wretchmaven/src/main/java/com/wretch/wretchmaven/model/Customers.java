16335703278561424b122151e5d4aaec34cb14bf
kim.stuber <kim.stuber@web.de>
2019-12-05 08:02:25 +0100

    database conn

---
+++ wretchmaven/src/main/java/com/wretch/wretchmaven/model/Customers.java
@@ -0,0 +1,27 @@
+package com.wretch.wretchmaven.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="Customers")
+public class Customers {
+ 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
+	private int id;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "lastname")
+	private String lastname;
+	
+}
