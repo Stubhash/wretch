d3e6ee4e1f78f0f7d2c3d657b419f4514ff6bff0
DanaGeßler <DanaGeler@Lenovo-DGE.speedport.ip>
2019-12-06 20:35:18 +0100

    Button can now change on-click

--- wretchmaven/src/main/resources/static/js/exchanging.js
+++ wretchmaven/src/main/resources/static/js/exchanging.js
@@ -5,8 +5,10 @@ exchanging.js works with the user input when the "Exchange now"-Button was press
 
 function modify_exchange_button()
 {
-    var exchange_button = document.getElementById("exchange");
-	if (exchange_button.value=="Exchange now") exchange_button.value = "Parcel sent";
-	else if (exchange_button.value=="Parcel sent") exchange_button.value = "Rate your Wretcher";
-	else exchange_button.value = "Exchange now";
+    var exchange_button = document.getElementById("exchange-button");
+	if (exchange_button.textContent=="Exchange now!") exchange_button.textContent = "Parcel sent";
+	else if (exchange_button.textContent=="Parcel sent") exchange_button.textContent = "Rate your Wretcher";
+	else exchange_button.textContent = "Exchange now!";
+	document.getElementById("exchange-button").textContent = exchange_button.textContent;
+	
 }
