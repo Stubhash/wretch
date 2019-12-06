8806f2f5946c1e92405c341faf2ba498ad76d5a6
danag <danag@DESKTOP-GDRB26C.speedport.ip>
2019-12-06 16:25:55 +0100

    update

--- wretchmaven/src/main/resources/static/js/exchanging.js
+++ wretchmaven/src/main/resources/static/js/exchanging.js
@@ -8,5 +8,5 @@ function modify_exchange_button()
     var exchange_button = document.getElementById("exchange");
 	if (exchange_button.value=="Exchange now") exchange_button.value = "Parcel sent";
 	else if (exchange_button.value=="Parcel sent") exchange_button.value = "Rate your Wretcher";
-    else exchange_button.value = "Exchange now";
+	else exchange_button.value = "Exchange now";
 }
