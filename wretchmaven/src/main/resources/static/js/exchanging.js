dd2a15720e29f1674ce5815d35b1eaeedca00793
danag <danag@DESKTOP-GDRB26C.speedport.ip>
2019-12-07 14:18:07 +0100

    Exchange button läuft!

--- wretchmaven/src/main/resources/static/js/exchanging.js
+++ wretchmaven/src/main/resources/static/js/exchanging.js
@@ -1,13 +1,19 @@
 /*!
 exchanging.js works with the user input when the "Exchange now"-Button was pressed.
  */
-
+function hide_rating_buttons(){		
+ 		document.getElementById('rating_button1').style.visibility='hidden';
+		document.getElementById('rating_button2').style.visibility='hidden';
+		document.getElementById('rating_button3').style.visibility='hidden';
+		document.getElementById('rating_button4').style.visibility='hidden';
+		document.getElementById('rating_button5').style.visibility='hidden';
+}
 
 function modify_exchange_button()
 {
-    var exchange_button = document.getElementById("exchange-button");
+	var exchange_button = document.getElementById("exchange-button");
 	if (exchange_button.textContent=="Exchange now!") {
-		exchange_button.textContent = "Parcel sent";
+		exchange_button.textContent = "Parcel sent!";
 		var p1 = document.createElement("P");
 		var p2 = document.createElement("P");
 		var p3 = document.createElement("P");
@@ -20,12 +26,40 @@ function modify_exchange_button()
 		document.getElementById("address_text").appendChild(p2);
 		document.getElementById("address_text").appendChild(p3);
 		document.getElementById("address_text").appendChild(p4);
+
+		document.getElementById('rating_button1').style.visibility='hidden';
+		document.getElementById('rating_button2').style.visibility='hidden';
+		document.getElementById('rating_button3').style.visibility='hidden';
+		document.getElementById('rating_button4').style.visibility='hidden';
+		document.getElementById('rating_button5').style.visibility='hidden';
+
+
+
 	}
-	else if (exchange_button.textContent=="Parcel sent") {
-		exchange_button.textContent = "Rate your Wretcher";
+	
+	else if (exchange_button.textContent=="Parcel sent!") {
+		exchange_button.textContent = "Wretcher rated!";
 		document.getElementById("address_text").textContent = " ";
+
+		document.getElementById('rating_button1').style.visibility='visible';
+		document.getElementById('rating_button2').style.visibility='visible';
+		document.getElementById('rating_button3').style.visibility='visible';
+		document.getElementById('rating_button4').style.visibility='visible';
+		document.getElementById('rating_button5').style.visibility='visible';
+
+
 	}
-	else exchange_button.textContent = "Exchange now!";
-	document.getElementById("exchange-button").textContent = exchange_button.textContent;
+	else {
+		exchange_button.textContent = "Exchange now!";
+		document.getElementById("exchange-button").textContent = exchange_button.textContent;
+
+		document.getElementById('rating_button1').style.visibility='hidden';
+		document.getElementById('rating_button2').style.visibility='hidden';
+		document.getElementById('rating_button3').style.visibility='hidden';
+		document.getElementById('rating_button4').style.visibility='hidden';
+		document.getElementById('rating_button5').style.visibility='hidden';
+	}
+
 	
 }
+
