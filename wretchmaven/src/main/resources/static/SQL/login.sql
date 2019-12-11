0b1db384d9c3087e2b34d7aaa4f207bc59b8ce70
niko_sauter <sauterniko@googlemail.com>
2019-12-11 16:57:17 +0100

    deleted folder sql

--- wretchmaven/src/main/resources/static/SQL/login.sql
+++
@@ -1,4 +0,0 @@
-create table login(
-    username char(40) REFERENCES customer(username), 
-    password char(40)
-)
--- wretchmaven/src/main/resources/static/SQL/tradingpartners.sql
+++
@@ -1,5 +0,0 @@
-   CREATE TABLE tradingpartners (
-    username char(40) REFERENCES customer(username), 
-    trading_partner char(40), 
-    FOREIGN KEY(trading_partner) REFERENCES customer(username)
-   )
