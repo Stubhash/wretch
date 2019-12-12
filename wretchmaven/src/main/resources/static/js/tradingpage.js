7f72b662018a8e6321d4da66abc6b2a6283fab1d
DanaGeßler <DanaGeler@Lenovo-DGE>
2019-12-12 20:29:05 +0100

    tradingpage js

---
+++ wretchmaven/src/main/resources/static/js/tradingpage.js
@@ -0,0 +1,16 @@
+var app = angular.module("setPoints", []);
+app.controller("points", function($http, $scope) {
+	var controller = this;
+
+	
+
+	controller.updatePoints = [];
+	$scope.updatePoints = function() {
+		if(One=ar points1= $scope.One;
+		
+		Userpoints
+		};
+
+		$http.post("/rating", Userpoints);
+	}
+});
